a29fec950b25135491bb3477339ccc37e473b12b
Simon Cozens <simon@simon-cozens.org>
2018-02-06 17:04:50 +1100

    Basic error handling

--- actions/command.js
+++ actions/command.js
@@ -4,6 +4,10 @@ const { exec } = require('child_process');
 actions["command"] = {
   "fire": function(w) {
     exec(w.command, (error, stdout, stderr) => {
+      if (error) {
+        auxiliary.log(`Error running ${w.command}: ${error}`);
+        return;
+      }
     })
   }
 }
